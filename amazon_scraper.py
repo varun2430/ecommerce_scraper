6a9e2d8192d456add4fc52b3d727032e4b50e709
Varun Kadkade <varunkadkade2442@gmail.com>
2022-04-07 14:45:00 +0530

    minor change

--- amazon_scraper.py
+++ amazon_scraper.py
@@ -109,15 +109,7 @@ class AmazonAPI:
 
 if __name__ == "__main__":
     
-    URL = [
-    "https://www.amazon.in/dp/1526651637/ref=s9_acsd_al_bw_c2_x_5_t?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-4&pf_rd_r=TAQ8R2DB8W3XXH6G019A&pf_rd_t=101&pf_rd_p=c94ab90f-f846-4a79-bfd4-9277e12942be&pf_rd_i=976389031",
-    "https://www.amazon.in/gp/product/B096VD213D/ref=s9_acss_bw_cg_WLM_3b1_w?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-6&pf_rd_r=5A3XDM8A18H5WGCJJHY2&pf_rd_t=101&pf_rd_p=899c625b-c7ed-4c88-8b7e-ae7bf2e7e58d&pf_rd_i=1389401031",
-    "https://www.amazon.in/Jack-Jones-Mens-T-Shirt-2422204036_Coral_Medium/dp/B099FDNH5F/ref=sr_1_3?pf_rd_i=6648217031&pf_rd_m=A1K21FY43GMZF8&pf_rd_p=0485126c-6a8c-4591-8258-48e87a7c150a&pf_rd_r=CX6NR4SF6YJ812M4Q3WM&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1645787291&refinements=p_36%3A-39900%2Cp_72%3A1318476031%2Cp_85%3A10440599031&rnid=10440598031&rps=1&s=apparel&sr=1-3",
-    "https://www.amazon.in/SanDisk-Ultra-Drive-Flash-128GB/dp/B084PJSSQ1/ref=Oct_d_obs_1375411031?pd_rd_i=B084PJSSQ1&pd_rd_r=e9ba37ba-2ee4-4ddb-829d-cbd1e08d5049&pd_rd_w=Y8nu4&pd_rd_wg=OSU3z&pf_rd_p=452e6737-1f7b-494e-b8b1-21478ecbd714&pf_rd_r=DZ0PAN1CFJ3DNJT4JS6Q",
-    "https://www.amazon.in/Casio-FX-991EX-Scientific-Calculator-Black/dp/B011UK5DGY/ref=zg-bs_office_4/262-2961657-4032542?pd_rd_w=AEip5&pf_rd_p=56cde3ad-3235-46d2-8a20-4773248e8b83&pf_rd_r=FJ980AQVQ9K7KKZYJFH4&pd_rd_r=5debcdbb-313f-4ab7-bdca-2c5960f28715&pd_rd_wg=tLKOx&pd_rd_i=B011UK5DGY&psc=1"
-    ]
+    URL = "https://www.amazon.in/MSI-IPS-Level-Windows-RX5500M-B5DD-077IN/dp/B09BRCHW4B?ref_=Oct_DLandingS_D_794a656e_76&smid=A14CZOWI0VEHLG"
 
-    for url in URL:
-        print("")
-        amz = AmazonAPI()
-        amz.get_product_info(url)
+    amz = AmazonAPI()
+    amz.get_product_info(URL)
